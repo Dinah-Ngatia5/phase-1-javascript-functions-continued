fc8622e732330bcd1e8f7ef99a5f4c739aa3acc3
+ <+>
2024-01-17 18:02:01 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun (activity1 = "roller-skate") {
+    return `This Saturday, I want to ${activity1}!`;
+}
+saturdayFun();
+function saturdayFun (activity1 = "roller-skate") {
+    return `This Saturday, I want to ${activity1}!`;
+}
+saturdayFun("bathe my dog");
+function mondayWork (task = "go to the office") {
+    return `This Monday, I will ${task}.`;
+}
+mondayWork("write a report");
+function wrapAdjective (visual = "*") {
+    return function (mine = "special") {
+        return `You are ${visual}${mine}${visual}!`;
+    };
+} 
+wrapAdjective("%")("a dedicated programmer");
